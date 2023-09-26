abb5a2040e7277df27b09b5eac6e414a9f39dd0e
Dan Miller <anime.dani@mail.ru>
2023-09-26 19:18:34 +0500

    Реализовал работу программы

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,19 +3,66 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText a, b, answer;
+    TextView opr, error;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         a = findViewById(R.id.editTextNumber);
         b = findViewById(R.id.editTextNumber2);
-        answer = findViewById(R.id.editTextNumber);
+        answer = findViewById(R.id.editTextNumber3);
         opr = findViewById(R.id.textView);
+        error = findViewById(R.id.textView2);
     }
 
-    public void plus_btn(View view)
+    public void btn_sum(View view) {
+        int a_number=Integer.parseInt(a.getText().toString());
+        int b_number=Integer.parseInt(b.getText().toString());
+        int c = a_number + b_number;
+        answer.setText(Integer.toString(c));
+        opr.setText("+");
+    }
+
+    public void btn_difference(View view) {
+        int a_number = Integer.parseInt(a.getText().toString());
+        int b_number = Integer.parseInt(b.getText().toString());
+        int c = a_number - b_number;
+        answer.setText(Integer.toString(c));
+        opr.setText("-");
+    }
+
+    public void btn_division(View view) {
+        int a_number = Integer.parseInt(a.getText().toString());
+        int b_number = Integer.parseInt(b.getText().toString());
+        if (b_number != 0) {
+            int c = a_number / b_number;
+            answer.setText(Integer.toString(c));
+            opr.setText("/");
+        }
+        else
+        {
+            answer.setText("ERROR");
+            error.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void btn_multiplication(View view) {
+        int a_number = Integer.parseInt(a.getText().toString());
+        int b_number = Integer.parseInt(b.getText().toString());
+        int c = a_number * b_number;
+        answer.setText(Integer.toString(c));
+        opr.setText("*");
+    }
+
+    public void btn_clean(View view){
+        a.setText("");
+        b.setText("");
+        answer.setText("");
+    }
 }
